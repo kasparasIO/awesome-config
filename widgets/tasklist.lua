eed28d25cef6be4d491e9fccc75cac32e5e120a5
Kasparas <git@kasparas.io>
2024-04-30 21:43:04 +0300

    taskbar image layout

--- widgets/tasklist.lua
+++ widgets/tasklist.lua
@@ -1,5 +1,6 @@
 local awful = require("awful")
 local gears = require("gears")
+local wibox = require("wibox")
 
 local tasklist_buttons = gears.table.join(
   awful.button({ }, 1, function (c)
@@ -12,24 +13,40 @@ local tasklist_buttons = gears.table.join(
         {raise = true}
       )
     end
-  end),
-  awful.button({ }, 3, function()
-    awful.menu.client_list({ theme = { width = 250 } })
-  end),
-  awful.button({ }, 4, function ()
-    awful.client.focus.byidx(1)
-  end),
-  awful.button({ }, 5, function ()
-    awful.client.focus.byidx(-1)
   end))
-
-
 
 local function create_tasklist(s)
     return awful.widget.tasklist {
         screen  = s,
         filter  = awful.widget.tasklist.filter.currenttags,
-        buttons = tasklist_buttons
+        buttons = tasklist_buttons,
+        widget_template = {
+            {
+                {
+                    {
+                        id = 'icon_role',
+                        widget = wibox.widget.imagebox,
+                        forced_height = 22,
+                        forced_width = 22,
+                    },
+                      widget = wibox.container.place,
+                      halign = "center",
+                      valign = "center",
+                },
+                  {
+                      margins = 4,
+                      widget = wibox.container.margin,
+                  },
+                {
+                    id = 'text_role',
+                    widget = wibox.widget.textbox,
+                },
+                layout = wibox.layout.fixed.horizontal,
+            },
+            left  = 12,
+            right = 12,
+            widget = wibox.container.margin
+        }
     }
 end
 
